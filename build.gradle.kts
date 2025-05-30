23c14045b039ff41b5290ff8a7a1cb4e267977dc
francis-ohara <francisohara@google.com>
2025-05-30 10:54:21 -0700

    build: add dependency for google vertexai java sdk

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.google.cloud:google-cloud-vertexai:1.23.0")
 }
 
 tasks.test {
